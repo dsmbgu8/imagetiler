bc09aac5d28b46cfa42b00990b23cb3639213c66
B. Bue <bbue@jpl.nasa.gov>
2017-01-10 10:12:35 -0800

    Major rewrite of imagetiler, now much faster and achieves better image coverage

--- imtile/imagetiler.py
+++ imtile/imagetiler.py
@@ -7,16 +7,16 @@ filterwarnings("ignore", message='.*is a low contrast image.*')
 
 tileinfof = 'tileinfo.txt'
 tileinfohdr = 'tileid row_start row_stop col_start col_stop percent_valid'
-def imslice2str(imslice):
+def tile2str(tileslice):
     """
-    converts 2d slice ((row_start,row_stop,None),(col_start,col_stop,None)) to
+    converts 3d tile slice ((row_start,row_stop,None),(col_start,col_stop,None)) to
     output string format='row_start row_stop col_start col_stop'
     """
-    return ' '.join(['%d %d'%(si.start,si.stop) for si in imslice])
+    return ' '.join(['%d %d'%(si.start,si.stop) for si in tileslice])
 
 class MaskTiler:
     """
-    MaskTiler(mask,tiledim,masked_reject=0.75,maxsearch=100,reinit_mask=True)
+    MaskTiler(mask,tiledim,reject=0.75,maxsearch=100,reinit_mask=True)
     
     Summary: generates randomly selected tiledim x tiledim subtiles given
     initial mask of valid pixel locations
@@ -26,18 +26,18 @@ class MaskTiler:
     - mask: [nrows x ncols] bool mask indicating valid regions of img
     
     Keyword Arguments:
-    - masked_reject: max percentage of masked pixels/tile to warrant rejection
+    - reject: max percentage of (mask==0)/ntilepix to warrant rejection
     
     Output:
-    - tileij = tiledim x tiledim tile extracted from img at position i,j
+    - tileij = tiledim x tiledim tiles extracted from img at position i,j
     """
 
     def __init__(self,mask,tiledim,numtiles,**kwargs):
         self.tiledim  = tiledim
         self.numtiles = numtiles
 
-        self.maxsearch = kwargs.pop('maxsearch',100)
-        self.masked_reject = kwargs.pop('masked_reject',0.75)
+        self.maxsearch = kwargs.pop('maxsearch',1000)
+        self.minvalid = kwargs.pop('min_valid',0.75)
         self.with_replacement = kwargs.pop('with_replacement',False)
         self.reinit_mask = kwargs.pop('reinit_mask',True) # reset mask if we run out of valid pixels
         self.verbose = kwargs.pop('verbose',False)
@@ -52,29 +52,33 @@ class MaskTiler:
             raise Exception(msg)
 
         # assign initial mask pixels + threshold
-        self.maskinit  = np.bool8(mask)
-        self.mask      = self.maskinit.copy()                
+        self.maskinit  = np.bool8(mask.copy())
+        self.mask      = self.maskinit.copy()
         
         # get pixel offsets from tile dim
-        self.rowdim    = self.tiledim-(self.nrows%self.tiledim)-1
-        self.coldim    = self.tiledim-(self.ncols%self.tiledim)-1
-        self.pixrc     = np.meshgrid(range(0,self.rowdim,5),
-                                     range(0,self.coldim,5))
+        self.rowdim    = self.nrows-(self.nrows%self.tiledim)
+        self.coldim    = self.ncols-(self.ncols%self.tiledim)
+        self.rowrange  = range(0,self.rowdim,5)
+        self.colrange  = range(0,self.coldim,5)
+        self.pixrc     = np.meshgrid(self.rowrange,self.colrange)
         self.pixrc     = np.c_[self.pixrc].reshape([2,-1]).T
         self.ntilepix  = self.rowdim*self.coldim
 
         # compute number of rows/cols of tiledim tiles
-        self.tilerows  = self.nrows//tiledim
-        self.tilecols  = self.ncols//tiledim
+        self.tilerows  = int(np.ceil(self.nrows/tiledim))
+        self.tilecols  = int(np.ceil(self.ncols/tiledim))
         self.tileij    = np.meshgrid(range(self.tilerows),range(self.tilecols))
         self.tileij    = np.c_[self.tileij].reshape([2,-1]).T
         self.ntiles    = self.tileij
 
         # keep track of tiles we've already collected
-        self.tiles = []        
-        self.minvalid  = (1.0-self.masked_reject)*self.ntilepix
+        self.tiles = []
+        if self.minvalid < 1.0:
+            self.minvalid  = int(round(self.minvalid*self.ntilepix))
 
-    def next_tile(self):
+        self.collect()
+
+    def next(self):
         # randomly selects a tile from the list of valid pixel/tile offsets
         # while preserving state, allows for sampling with/without replacement
         # returns best tile slice and percent of valid pixels for best slice
@@ -83,54 +87,66 @@ class MaskTiler:
         tijbest = None # 
         tijvalid = 0.0 # 
 
-        if len(self.pixrc)>0 and len(self.tileij)>0:
-            # randomly select next tile
-            nsearch = 0
-            pixrc  = list(self.pixrc)            
-            while pixrc != []:
-                # pick a random row/col pixel offset from our valid pixel list
-                r,c = pixrc.pop(np.random.randint(len(pixrc)))
-                tileij = list(self.tileij)
-                # search tiles in random order for current pixel offset
-                while tileij != []:
-                    ti,tj = tileij.pop(np.random.randint(len(tileij)))
-                    i,j = (ti*self.tiledim)+r,(tj*self.tiledim)+c
-                    if i+self.tiledim>=self.nrows or j+self.tiledim>=self.ncols:
-                        continue
+        tr,tc = self.tilerows,self.tilecols
+        pr,pc = self.rowdim,self.coldim
+        if len(self.pixrc)==0 or len(self.tileij)==0:
+            warn('no pixel offsets defined, cannot proceed')
+            return (tijbest, tijvalid)
+        
+        nsearch = 0
+        pixrc  = list(self.pixrc)
+        r,c = pixrc.pop(np.random.randint(len(pixrc)))            
+        while nsearch <= self.maxsearch:
+            # pick a random row/col pixel offset from our valid pixel list
 
-                    tij = (slice(i,i+self.tiledim,None),
-                           slice(j,j+self.tiledim,None))
-                    
-                    # select tile with the most valid (mask==True) pixels
-                    nvalid = np.count_nonzero(self.mask[tij])
-                    if nvalid>tijvalid:
-                        tijbest = tij
-                        tijvalid = nvalid                        
-                        if nvalid>self.minvalid:
-                            # exit early if we meet stopping criteria
-                            nsearch = self.maxsearch
-                            break
+            tileij = list(self.tileij)
+            # search tiles in random order for current pixel offset
+            while tileij != []:
+                ti,tj = tileij.pop(np.random.randint(len(tileij)))
+                i,j = (ti*self.tiledim)+r,(tj*self.tiledim)+c
+                if i+self.tiledim>=self.nrows or j+self.tiledim>=self.ncols:
+                    continue
 
-                # found an acceptable tile or all pixels masked invalid
-                if nsearch>=self.maxsearch:
-                    # reset mask if we run out of valid pixels
-                    coverage = 100*(1-tijvalid/self.ntilepix)
-                    if self.reinit_mask and coverage > 95:
-                        if self.verbose:
-                            msg = "Reinitializing mask (%5.3f%% pixel coverage)"%coverage
-                            warn(msg)
-                        self.mask = self.maskinit.copy()
-                        tijbest = None
-                        tijvalid = 0.0
-                        nsearch = 0
-                    else:
-                        # found a good tile, mask if sampling wo replacement
-                        if not self.with_replacement:
-                            self.mask[tijbest] = False
+                tij = (slice(i,i+self.tiledim,None),
+                       slice(j,j+self.tiledim,None))
+
+                # select tile with the most valid (mask==True) pixels
+                nvalid = np.count_nonzero(self.mask[tij])
+                if nvalid>tijvalid:
+                    tijbest, tijvalid = tij, nvalid
+                    if nvalid>self.minvalid:
+                        # exit early if we meet stopping criteria
+                        nsearch = self.maxsearch
                         break
 
-                # need to keep track of searches to avoid infinite loop
-                nsearch += 1
+            # found an acceptable tile or all pixels masked invalid (reset or exit)
+            if nsearch>=self.maxsearch:
+                # reset mask if we run out of valid pixels
+                coverage = (100*(1.0-tijvalid))/self.ntilepix
+                if self.reinit_mask and coverage > 95:
+                    if self.verbose:
+                        msg = "Reinitializing mask (%5.3f%% pixel coverage)"%coverage
+                        warn(msg)
+                    self.mask = self.maskinit.copy()
+                    tijbest, tijvalid = None, 0.0
+                    nsearch = 0
+                    r,c = pixrc.pop(np.random.randint(len(pixrc)))
+                else:
+                    # found a good tile, mask if sampling wo replacement
+                    if not self.with_replacement:
+                        self.mask[tijbest] = False
+                break
+
+            # update either the row or the column, but not both
+            if np.random.rand() > 0.5:
+                rr = self.rowrange[np.random.randint(len(self.rowrange))]
+                r = (r+rr)%self.rowdim
+            else:
+                cc = self.colrange[np.random.randint(len(self.colrange))]
+                c = (c+cc)%self.coldim
+                
+            # keep track of searches to avoid infinite loop
+            nsearch += 1
                     
         return (tijbest, tijvalid)
 
@@ -145,12 +161,12 @@ class MaskTiler:
         if self.verbose:
             print(tileinfohdr)
         for i in range(self.numtiles):
-            tij,tijvalid = self.next_tile()
+            tij,tijvalid = self.next()
             if tij==None:
                 break
             tijpercent = tijvalid/self.ntilepix
             if self.verbose:
-                print(i,imslice2str(tij),'%4.3f'%tijpercent)
+                print(i,tile2str(tij),'%4.3f'%tijpercent)
                 
             tiles.append(tij)
             percent_valid.append(tijpercent)
@@ -160,34 +176,46 @@ class MaskTiler:
         numtiles = len(tiles)
         print('Collected',numtiles,'of',self.numtiles,'requested tiles')
         self.numtiles = numtiles
-
-    @timeit
-    def generate(self,img):
-        self.collect()
-        for tij in self.tiles:
-            yield img[tij]
     
     @timeit
-    def save_tiles(self,savefunc,tiledir,basename,**kwargs):
+    def save_tiles(self,img,savefunc,outdir,**kwargs):
+        import pylab as pl
+        import matplotlib.patches as patches
         overwrite = kwargs.pop('overwrite',False)
-        outext = kwargs.pop('outext','.jpg')
-
         outfile = None
-        imgtiles = self.extract()        
         logmsg = [tileinfohdr]
+        figi = pl.figure()
+        axi = figi.add_subplot(111)
+        axi.imshow(img)
+        figm = pl.figure()
+        axm = figm.add_subplot(111)
+        axm.imshow(np.uint8(self.maskinit))        
         for i,tij in enumerate(self.tiles):
-            tijid,tijslstr = 'tile%d'%i,imslice2str(tij)
-            tijimg,tijvalid = imgtiles[i],self.percent_valid[i]
+            tijid,tijslstr = 'tile%d'%i,tile2str(tij)
+            tijimg,tijvalid = img[self.tiles[i]],self.percent_valid[i]
+            print(tijimg[:,:,0].min(),tijimg[:,:,0].max(),tijimg.shape)
             logmsg.append(' '.join([tijid,tijslstr,'%4.3f'%tijvalid]))
-            outfile = savefunc(tiledir,tileid,basename,outext,tijimg)
+            savefunc(pathjoin(outdir,tijid),tijimg)
+            pi = patches.Rectangle((tij[1].start,tij[0].start),
+                                  self.tiledim,self.tiledim,
+                                  fill=False,edgecolor='g',
+                                  linewidth=2)
+            pm = patches.Rectangle((tij[1].start,tij[0].start),
+                                  self.tiledim,self.tiledim,
+                                  fill=False,edgecolor='g',
+                                  linewidth=2)            
+            axi.add_patch(pi)
+            axm.add_patch(pm)
 
-        if outfile:
-            outdir = dirname(outfile)
-            print('Saved',self.numtiles,'tiles to',outdir)
-            masku8 = 255*self.mask.astype(np.uint8)
-            maskinitu8 = 255*self.maskinit.astype(np.uint8)
-            savefunc(tiledir,'mask',basename,outext,masku8)
-            savefunc(tiledir,'maskinit',basename,outext,maskinitu8)
-            with open(pathjoin(outdir,tileinfof),'w') as fid:
-                print('\n'.join(logmsg),file=fid)
+        print('Saved',self.numtiles,'tiles to',outdir)
+        savefunc(pathjoin(outdir,'mask'),self.mask)
+        savefunc(pathjoin(outdir,'maskinit'),self.maskinit)
+        with open(pathjoin(outdir,tileinfof),'w') as fid:
+            print('\n'.join(logmsg),file=fid)
 
+
+        pl.figure()
+        pl.imshow(np.uint8(self.mask))
+
+        pl.show()
+                
